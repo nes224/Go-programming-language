6a7002dd79a6fd70ddd1484cf92d7261d5a275be
nes224 <nes224@hotmail.com>
2024-05-18 16:04:19 +0700

    deadlock

--- concurrent/goroutine_3/goroutine_3.go
+++ concurrent/goroutine_3/goroutine_3.go
@@ -7,14 +7,14 @@ import (
 
 func goroutine1(c chan bool){
 	fmt.Println("Goroutines #1 has started, waiting for Goroutines #2 to start")
-	<- c
+	<- c // read from channel
 	fmt.Println("Goroutines #1 received a notification from Gourtines #2")
 }
 
 func goroutine2(c chan bool) {
 	fmt.Println("Goroutines #2 has started, do some work and notify Gorutines #1")
 	time.Sleep(2 * time.Second)
-	c <- true 
+	c <- true  // write to channel
 	fmt.Println("Goroutine #2 has finished")
 }
 
