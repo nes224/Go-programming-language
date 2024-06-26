112fd33e907e170d37659e8ab781987b5c595b61
nes224 <nes224@hotmail.com>
2024-06-26 09:55:00 +0700

    linkedlist

---
+++ linkedlist/linked_list/iterate.list.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func (linkedList *LinkedList) IterateList() {
+	var node *Node
+	for node = linkedList.headNode; node != nil; node = node.nextNode {
+		fmt.Println(node.property)
+	}
+}
