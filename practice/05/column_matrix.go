70ef52e754a7df41be4465afd2b90b5076e5d0bb
Nes224 <Nes224@hotmail.com>
2024-07-16 09:07:35 +0700

    column_matrix

---
+++ practice/05/column_matrix.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func columnMatrix() {
+	var matrix = [4][1]int{{1},{2},{3},{4}}
+
+	fmt.Println(matrix)
+}
--- practice/05/main.go
+++ practice/05/main.go
@@ -1,5 +1,6 @@
 package main
 
 func main() {
-	boolMatrix()
+	// boolMatrix()
+	columnMatrix()
 }
