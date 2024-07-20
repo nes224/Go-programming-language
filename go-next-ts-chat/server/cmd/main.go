c347065dd221ab5f8435c3be033f8fe2718255d8
Nes224 <Nes224@hotmail.com>
2024-07-20 17:45:55 +0700

    go_chat_2

--- go-next-ts-chat/server/cmd/main.go
+++ go-next-ts-chat/server/cmd/main.go
@@ -4,12 +4,20 @@ import (
 	"log"
 
 	"github.com/gochat/db"
+	"github.com/gochat/internal/user"
+	"github.com/gochat/router"
 )
 
 func main() {
-	_, err := db.NewDatabase()
+	dbConn, err := db.NewDatabase()
 	if err != nil {
 		log.Fatalf("could not initialize database connection: %s", err)
 	}
 
+	userRep := user.NewRepository(dbConn.GetDB())
+	userSvc := user.NewService(userRep)
+	userHandler := user.NewHandler(userSvc)
+
+	router.InitRouter(userHandler)
+	router.Start("0.0.0.0:8090")
 }
--- go-next-ts-chat/server/internal/user/user_service.go
+++ go-next-ts-chat/server/internal/user/user_service.go
@@ -6,7 +6,10 @@ import (
 	"time"
 
 	"github.com/gochat/util"
+	"github.com/golang-jwt/jwt/v4"
 )
+
+const secretKey = "secret"
 
 type service struct {
 	Repository
@@ -24,7 +27,6 @@ func (s *service) CreateUser(c context.Context, req *CreateUserReq) (*CreateUser
 	ctx, cancel := context.WithTimeout(c, s.timeout)
 	defer cancel()
 
-	// TODO: hash password
 	hashedPassword, err := util.HashPassword(req.Password)
 	if err != nil {
 		return nil, err
@@ -48,4 +50,41 @@ func (s *service) CreateUser(c context.Context, req *CreateUserReq) (*CreateUser
 	}
 
 	return res, nil
+}
+
+type MyJWTClaims struct {
+	ID       string `json:"id"`
+	Username string `json:"username"`
+	jwt.RegisteredClaims
+}
+
+func (s *service) Login(c context.Context, req *LoginUserReq) (*LoginUserRes, error) {
+	ctx, cancel := context.WithTimeout(c, s.timeout)
+	defer cancel()
+
+	u, err := s.Repository.GetUserByEmail(ctx, req.Email)
+	if err != nil {
+		return &LoginUserRes{}, nil
+	}
+
+	// err = util.CheckPassword(req.Password, u.Password)
+	// if err != nil {
+	// 	return &LoginUserRes{}, err
+	// }
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, MyJWTClaims{
+		ID:       strconv.Itoa(int(u.ID)),
+		Username: u.Username,
+		RegisteredClaims: jwt.RegisteredClaims{
+			Issuer:    strconv.Itoa(int(u.ID)),
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(24 * time.Hour)),
+		},
+	})
+
+	ss, err := token.SignedString([]byte(secretKey))
+	if err != nil {
+		return &LoginUserRes{}, err
+	}
+
+	return &LoginUserRes{accessToken: ss, Username: u.Username, ID: strconv.Itoa(int(u.ID))}, nil
 }
